1c5043d47cf215486dd950b3dd244d7e4b765b15
Ethan Anderson <ethanandy90@gmail.com>
2024-05-07 13:35:59 -0600

    set up initial routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/Place";
 
 function App() {
-  return <h1>Lets start!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Users />} />
+        <Route path="/places/new" exact element={<NewPlace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
