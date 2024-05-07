9024fd52821d3ebbcdac05272a97dd1b6ca8e976
Ethan Anderson <ethanandy90@gmail.com>
2024-05-07 15:33:46 -0600

    use portals to open and close mobile sidebar

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
+import Backdrop from "../UIElements/Backdrop";
 
 export default function MainNavigation() {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  function handleDrawer() {
+    setDrawerIsOpen(!drawerIsOpen);
+  }
+
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={handleDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={handleDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 }
