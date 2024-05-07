83ae5fc31f5f5e97a43fef58e3ab668d57fa9cdc
Ethan Anderson <ethanandy90@gmail.com>
2024-05-07 14:11:18 -0600

    create a dummy user item, style that item with components and css

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,3 +1,30 @@
 import React from "react";
 
-export default function UsersList() {}
+import "./UsersList.css";
+import UserItem from "./UserItem";
+
+export default function UsersList(props) {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found.</h2>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places}
+          />
+        );
+      })}
+    </ul>
+  );
+}
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 export default function Users() {
-  return <h2>Users is working!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Jean-Baptiste Emanuel Zorg",
+      image:
+        "https://i.pinimg.com/originals/2c/4c/25/2c4c259c3b58801be7203e5ec2fd7b65.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 }
